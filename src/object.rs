50eacf876ffacca088fa149c8722ce5f0930ff03
robert-bn <26419016+robert-bn@users.noreply.github.com>
2022-06-05 02:11:29 +0100

    Added comment

--- src/object.rs
+++ src/object.rs
@@ -5,5 +5,6 @@ pub trait Object {
     fn intersection(&self, ray: &Ray<f64>) -> Option<f64>;
 
     /// Unit vector normal on object at intersection
+    /// This vector points outward on a closed object
     fn normal(&self, intersection: &Vec3<f64>) -> Vec3<f64>;
 }
